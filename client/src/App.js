dbaa3c368261ac3b2f2e58e9de37ad8e4f4f4a99
Alamin <alaminprogramerr@gmail.com>
2021-04-30 00:52:27 -0700

    initialcommit

---
+++ client/src/App.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Redirect } from 'react-router-dom';
+
+import PublicRoute from './Component/util/PublicRoute'
+import ProtectedRoute from './Component/util/ProtectedRoute'
+
+import CreatePost from './Component/ProtectedComponent/CreatePost'
+
+
+
+import Login from './Component/PublicComponent/Login'
+import Register from './Component/PublicComponent/Register'
+import EditPost from './Component/ProtectedComponent/EditPost';
+import Dashboard from './Component/ProtectedComponent/Dashboard';
+
+class App extends Component {
+    render() { 
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <ProtectedRoute path="/home" component={Dashboard} />
+                    <ProtectedRoute path="/create-post" component={CreatePost} />
+                    <ProtectedRoute path="/edit-post" component={EditPost} />
+
+
+
+
+
+                    <PublicRoute  path='/dashboard' component={Dashboard}/>
+                    <PublicRoute  path='/login' component={Login}/>
+                    <PublicRoute  path='/register' component={Register}/>
+                    <Redirect from="/" to="/home"/>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
+ 
+export default App;
